51bd50b92591d41fd4e9606b016866c8a26a55ab
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-05-22 14:17:38 -0300

    connect with mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://tomasesquivel:x5lUq4i8WBiwTSrw@cluster0.avyewm6.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
-import express from 'express';
-import { PORT } from './config.js';
+import express, { response } from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.get('/', (request, response) => {
+  console.log('Request received');
+  return response.status(234).send('Hello, World!');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
 
+mongoose.connect(mongoDBURL)
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
