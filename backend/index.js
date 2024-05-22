866db89ca5b28b60f01a6a1446e9b25923c7a557
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-05-22 16:01:17 -0300

    create book model

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,37 @@
 import express, { response } from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
   console.log('Request received');
   return response.status(234).send('Hello, World!');
 });
 
+app.post('/books', async (request, response) => {
+  try{
+    const { title, author, publishYear } = request.body;
+
+    if (!title || !author || !publishYear) {
+      return response.status(400).send('Title, author, and publishYear are required');
+    }
+    const book = new Book({ title, author, publishYear });
+    await book.save();
+    return response.status(201).send(book);
+  }catch(error){
+    console.error('Error occurred:', error);
+    return response.status(500).send('Internal Server Error');
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
 
 mongoose.connect(mongoDBURL)
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
